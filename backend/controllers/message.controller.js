fb8329c525e39dd9be309a708ad0330759423eac
emil <me@fakemail.com>
2024-11-19 20:35:21 +0200

    Fix message sending

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,31 +1,38 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
 
-export async function sendMessage(req, res) {
+export const sendMessage = async (req, res) => {
   try {
     const { message } = req.body;
-    const { id: recieverId } = req.params;
+    const { id: receiverId } = req.params;
     const senderId = req.user._id;
 
-    const conversation = await Conversation.findOne({
-      participants: { $all: [senderId, recieverId] },
+    let conversation = await Conversation.findOne({
+      participants: { $all: [senderId, receiverId] },
     });
 
     if (!conversation) {
       conversation = await Conversation.create({
-        participants: [senderId, recieverId],
+        participants: [senderId, receiverId],
       });
     }
+
     const newMessage = new Message({
-      senderId: senderId,
-      recieverrId: recieverId,
-      message: message,
+      senderId,
+      receiverId,
+      message,
     });
+
     if (newMessage) {
       conversation.messages.push(newMessage._id);
     }
+
+    // this will run in parallel
+    await Promise.all([conversation.save(), newMessage.save()]);
+
     res.status(201).json(newMessage);
   } catch (error) {
+    console.log("Error in sendMessage controller: ", error.message);
     res.status(500).json({ error: "Internal server error" });
   }
-}
+};
--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 app.use(express.json()); //to prase incomming reqeuests with JSON payloads (req.body)
-app.use(cookieParser);
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
