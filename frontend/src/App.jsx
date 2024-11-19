5ca196a01f935ecd4aea6fd30b9140bacaaa179f
emil <me@fakemail.com>
2024-11-19 21:24:14 +0200

    Implement signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import Login from "./pages/login/Login";
+import Signup from "./pages/signup/SignUp";
 import "./App.css";
 
 function App() {
   return (
     <div className="p-4 screen flex items-caenter justify-center">
-      <Login />
+      <Signup />
     </div>
   );
 }
