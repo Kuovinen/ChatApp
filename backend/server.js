61a804f4a244726a0587c6e10fba01f6f707fadc
emil <me@fakemail.com>
2024-11-19 20:55:11 +0200

    Implement getting users

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import connnectToMongoDB from "./db/connectToMongoDB.js";
 import cookieParser from "cookie-parser";
 const app = express();
@@ -14,6 +16,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 /*
 app.get("/", (req, res) => {
   res.send("Hello!!");
